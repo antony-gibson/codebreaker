f1cc4467960a3714a15910b33cc3e0c2cfa5d364
antonygibson <ang80@gmail.com>
2026-03-11 17:45:26 +0000

    Hopefully will upload

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,21 @@
 public class Main {
-    private String userInput;
+    //private String userInput;
     //take inspo from Application.java of MonsterAndTreasuresV4
-    public Main(String userInput) {
+    //public Main(String userInput) {
+    //}
+
+    public String getUserInput(){
         CleanText cleanText = new CleanText();
         System.out.print("Enter your Cipher: ");
         cleanText.toString();
         System.out.print("You chose ");
         System.out.println(cleanText.toString());
     }
+
+    public static void main(String[] args){
+        Main main = new Main(args[0]);
+        main.getUserInput();
+    }
+
+
 }
