ab01b9a8c6ea20a95921b15c4e95b5b4dc6ceb75
antonygibson <ang80@gmail.com>
2026-03-11 13:57:42 +0000

    Initial Commit: Getting Git to Work

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,10 @@
 public class Main {
     private String userInput;
     //take inspo from Application.java of MonsterAndTreasuresV4
-    public Main(){
+    public Main(String userInput){
+       CleanText cleanText = new CleanText();
        System.out.print("Enter your Cipher: ");
-       CleanText.toString();
+       cleanText.toString();
     }
 
 
