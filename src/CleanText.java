16c34c09713af0996e13ea251f0a0ff7b60e69e0
antonygibson <ang80@gmail.com>
2026-03-12 09:39:53 +0000

    First Working Code - takes user input and capitalises

--- src/CleanText.java
+++ src/CleanText.java
@@ -11,7 +11,7 @@ public class CleanText {
         userInput = input.nextLine();
         newUserInput = userInput.trim().toUpperCase();
         strippedUserInput = newUserInput.replaceAll("\\s", ""); //double slash allows all whitespace to be removed. It doesn't work without the double slash - if it ain't broke don't fix it.
-        onlyTextInput = strippedUserInput.replaceAll("[^A-Z, 0-9]", ""); //^ is 'not', so anything that isn't a letter or number gets removed
+        onlyTextInput = strippedUserInput.replaceAll("[^A-Z]", ""); //^ is 'not', so anything that isn't a letter or number gets removed
         System.out.println(newUserInput); //shows user capitalised input minus spaces before or after words
         System.out.println(strippedUserInput); //shows user capitalised input with no spaces at all
         System.out.println(onlyTextInput); //shows only letters or numbers of the input
