a99451c13b874709cd41f53ed693fcdb77489807
antonygibson <ang80@gmail.com>
2026-03-18 10:28:32 +0000

    Second Major Upload: Text Menu created

--- src/CleanText.java
+++ src/CleanText.java
@@ -10,11 +10,13 @@ public class CleanText {
     public String getInput(){
         userInput = input.nextLine();
         newUserInput = userInput.trim().toUpperCase();
+        return newUserInput;
+    }
+
+    public String getCipherInput(){
+        getInput();
         strippedUserInput = newUserInput.replaceAll("\\s", ""); //double slash allows all whitespace to be removed. It doesn't work without the double slash - if it ain't broke don't fix it.
         onlyTextInput = strippedUserInput.replaceAll("[^A-Z]", ""); //^ is 'not', so anything that isn't a letter or number gets removed
-        System.out.println(newUserInput); //shows user capitalised input minus spaces before or after words
-        System.out.println(strippedUserInput); //shows user capitalised input with no spaces at all
-        System.out.println(onlyTextInput); //shows only letters or numbers of the input
         return onlyTextInput;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,40 @@
 public class Main {
+    private String userMenuInput;
 
     public String getUserInput(){
         CleanText cleanText = new CleanText();
-        System.out.print("Enter your Cipher: ");
         String userInput = cleanText.getInput();
-        System.out.print("You chose ");
-        System.out.println(userInput);
         return userInput;
     }
 
+    public String getCipherInput(){
+        CleanText cleanText = new CleanText();
+        String userCipherInput = cleanText.getCipherInput();
+        return userCipherInput;
+    }
+
+    public void displayMenu(){
+        System.out.print("Ello Ello ELlo. Welcome.");
+        System.out.println("Please enter the number relating to the choice you are picking:");
+        System.out.println("1. Pick your Cipher");
+        System.out.println("2. Edit Key");
+        System.out.println("3. Display Key");
+        System.out.println("4. Input a file");
+        System.out.println("5. Display File");
+        System.out.println("6. Encrypt File");
+        System.out.println("7. Display Cipher Text");
+        System.out.println("8. Save Cipher Text");
+        System.out.println("9. Input Encrypted File");
+        System.out.println("10. Decrypt Encrypted File");
+        System.out.println("0. Exit Program");
+        userMenuInput = getUserInput();
+        System.out.println("You picked: " + userMenuInput + ". Good Move.");
+    }
 
     //below is what allows this file to be run by IntelliJ
     public static void main(String[] args){
         Main main = new Main();
+        main.displayMenu();
         main.getUserInput();
     }
 }
