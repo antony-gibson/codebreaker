f62b86c31d3e1963534490c06dd04137cb2d0144
antonygibson <ang80@gmail.com>
2026-03-20 11:07:03 +0000

    Code Breaker Assignment: Upload 2

--- src/Main.java
+++ src/Main.java
@@ -26,13 +26,13 @@ public class Main {
         System.out.println("1. Pick your Cipher");
         System.out.println("2. Edit Key");
         System.out.println("3. Display Key");
-        System.out.println("4. Input a file");
-        System.out.println("5. Display File");
+        System.out.println("4. Input Plaintext File");
+        System.out.println("5. Display Prepared Plaintext File");
         System.out.println("6. Encrypt File");
         System.out.println("7. Display Cipher Text");
         System.out.println("8. Save Cipher Text");
-        System.out.println("9. Input Encrypted File");
-        System.out.println("10. Decrypt Encrypted File");
+        System.out.println("9. Input Ciphertext File");
+        System.out.println("10. Decrypt Ciphertext File");
         System.out.println("0. Exit Program");
     }
 
@@ -41,6 +41,7 @@ public class Main {
         System.out.println("1. Caesar Cipher");
         System.out.println("2. Keyed Caesar Cipher");
         System.out.println("3. Vigenere Cipher");
+        System.out.println("0. Exit Program");
     }
 
     public void displayMenu(){
@@ -55,7 +56,12 @@ public class Main {
         cipherMenu();
         userCipherInput = getNumberInput();
         System.out.println("You picked: " + userCipherInput + ". Outstanding Choice.");
-        //return userCipherInput;
+
+        if (userCipherInput.equals("0")){
+            System.exit(0);
+        }
+
+        displayMenu();
     }
 
     public void menuActions(){
@@ -67,8 +73,8 @@ public class Main {
        } else {
            System.out.println("L bozo");
        }
-
     }
+
     //below is what allows this file to be run by IntelliJ
     public static void main(String[] args){
         Main main = new Main();
